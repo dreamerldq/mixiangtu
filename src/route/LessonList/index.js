d44e53dc0e6d99917d469862502a811620803f68
ZhangJinda96 <129920444@qq.com>
2019-01-23 19:34:05 +0800

    添加课程列表页面

--- src/route/LessonList/index.js
+++ src/route/LessonList/index.js
@@ -1,14 +1,43 @@
 import React from 'react'
+import LessonBlock from '../../components/LessonBlock/index.js'
+import './index.css'
+const datas = new Array(6).fill( {
+    lesson:'Scratch图形教学',
+    teacher: 'jingda',
+    type: '课堂教学',
+    age:'6-8岁'
+})
+
 export default class Index extends React.Component{
     // constructor(props){
     //     super(props);
     
     // }
+   
     render(){
         return(
-            <div>
-                这是课程页面
+            <div className="lesson_page">
+                <div className="lesson_name">
+                    Scratch
+                </div>
+                <div className="lesson_list">
+                {
+                    datas.map((item, index) => {
+                        return(
+                            <LessonBlock
+                                key={index}
+                                lesson={item.lesson}    
+                                teacher={item.teacher}
+                                type={item.type}
+                                age={item.age}
+                            >
+                            </LessonBlock>
+                        )
+                    })
+                } 
             </div>
+            </div>
+            
         )
     }
 }
