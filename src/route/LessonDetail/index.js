aa67174f084c1d3f33221e726ac3b3b792c3e35a
ZhangJinda96 <129920444@qq.com>
2019-01-28 16:22:31 +0800

    更新课程详情样式

--- src/route/LessonDetail/index.js
+++ src/route/LessonDetail/index.js
@@ -3,6 +3,7 @@ import './index.css'
 import DetailSide from '../../components/DetailSide/index.js'
 import DetailBody from '../../components/DetailBody/index.js'
 import DetailFrame from '../../components/DetailFrame/index.js'
+import { Button } from 'antd'
 
 const sideList = [
     '课程1',
@@ -17,8 +18,25 @@ export default class LessonDetail extends React.Component{
         return(
             
                 <DetailFrame>
-                    <div>
+                    <div className="lesson_detail_head">   
+                        <div className="head_left">
+                            <div className="head_titles">
+                                <div className="head_title">Scratch图形教学</div>
+                                <div className="head_subtitle">Scratch图形教学小猫钓鱼1</div> 
+                            </div>
+                            <div className="leasson_mode">
+                                授课模式：课堂教学 | 课程时长：2课时
+                            </div>
+                            <div className="teacher_info">
+                                <img/>
+                                <span>张哒哒</span>
+                            </div>
+                        </div>
 
+                        <div className="head_right">
+                                <div className="baoming">已报名<span>5</span>人</div>
+                                <Button type="primary">立即报名</Button>
+                        </div>
                     </div>
                     <DetailBody
                         title={'课程详情'}
