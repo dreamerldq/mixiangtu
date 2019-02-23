e5bed53f2282e5a1be6713cb54b8f8baa5a9ca13
ZhangJinda96 <129920444@qq.com>
2019-02-23 14:41:01 +0800

    build

---
+++ build/precache-manifest.2b8e3eb9329e30b94d8df3c75d601469.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "26d2e6bc630755a26fb6",
+    "url": "/static/css/main.57cab21c.chunk.css"
+  },
+  {
+    "revision": "26d2e6bc630755a26fb6",
+    "url": "/static/js/main.26d2e6bc.chunk.js"
+  },
+  {
+    "revision": "fb3fad7f9c6e490edd72",
+    "url": "/static/css/1.acf2787f.chunk.css"
+  },
+  {
+    "revision": "fb3fad7f9c6e490edd72",
+    "url": "/static/js/1.fb3fad7f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9312e9ea233d259693e8a98bdecba356",
+    "url": "/static/media/panorama_01.9312e9ea.jpg"
+  },
+  {
+    "revision": "9312e9ea233d259693e8a98bdecba356",
+    "url": "/static/media/panorama_02.9312e9ea.jpg"
+  },
+  {
+    "revision": "9312e9ea233d259693e8a98bdecba356",
+    "url": "/static/media/panorama_03.9312e9ea.jpg"
+  },
+  {
+    "revision": "9312e9ea233d259693e8a98bdecba356",
+    "url": "/static/media/panorama_04.9312e9ea.jpg"
+  },
+  {
+    "revision": "18b5a474dc08f1c5f77305ab70251802",
+    "url": "/index.html"
+  }
+];
